0fc06488c4394cbe06edadb76eba3a9dd1496a67
natan.chachko <natan.chachko@sigma.software>
2024-03-23 17:25:53 +0200

    refactor

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import dash
 import plotly.graph_objs as go
 from dash import dcc, html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import joblib
@@ -29,55 +30,131 @@ y = df['УСЬОГО']
 sc = StandardScaler()
 sc.fit_transform(x[best_features])
 
-app = dash.Dash()
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+
+def dropdown_section():
+    return html.Div([
+        dcc.Dropdown(
+            id='specialities-dropdown',
+            options=[{'label': spec, 'value': spec} for spec in specialities['spec_full']],
+            value=specialities['spec_full'].iloc[91],
+            style={'width': '500px', 'margin': 'auto', 'border-radius': '5px'},
+            placeholder='Оберіть спеціальність'),
+    ], style={'text-align': 'center', 'margin-top': '20px', 'justify-content': 'center'})
+
+
+def input_fields_section():
+    style = {'display': 'inline-block', 'margin': '10px', 'width': '150px', 'vertical-align': 'middle'}
+    return html.Div(
+        [
+            dbc.FormFloating(
+                [
+                    dbc.Input(id='podano_zayav', type='number', value=64, placeholder=''),
+                    dbc.Label("Подано заяв")
+                ],
+                style=style
+            ),
+            dbc.FormFloating(
+                [
+                    dbc.Input(id='uni_code', type='number', value=28, placeholder=''),
+                    dbc.Label("Код ЗВО")
+                ],
+                style=style
+            ),
+            dbc.FormFloating(
+                [
+                    dbc.Input(id='max_val', type='number', value=43, placeholder=''),
+                    dbc.Label("Макс. обсяг держзамовлення")
+                ],
+                style={'display': 'inline-block', 'margin': '8px', 'width': '250px', 'vertical-align': 'middle'}
+            ),
+            dbc.FormFloating(
+                [
+                    dbc.Input(id='super_val', type='number', value=11328, placeholder=''),
+                    dbc.Label("Суперобсяг")
+                ],
+                style=style
+            ),
+            dbc.FormFloating(
+                [
+                    dbc.Input(id='kvota', type='number', value=0, placeholder=''),
+                    dbc.Label("Квота-1")
+                ],
+                style=style
+            )
+        ],
+        style={'text-align': 'center', 'margin': '10px', 'justify-content': 'center'},
+    )
+
+
+def button_section():
+    return html.Div([
+        html.Button('Спрогнозувати', id='predict-button', className='floating-button')],
+        style={'text-align': 'center', 'justify-content': 'center', 'margin-bottom': '10px'})
+
+
+def prediction_output_section():
+    return html.Div(
+        id='prediction-output-container',
+        style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'},
+        children=[
+            html.Div(id='prediction-output')
+        ]
+    )
+
+
+def plots_table_section():
+    return html.Table(
+        id='plots-table',
+        style={'display': 'inline-block'},
+        children=[
+            html.Tr([
+                html.Td([
+                    dcc.Graph(id='prediction-plot-total')
+                ], style={'width': '45%', 'height': '35%'}),
+                html.Td([
+                    dcc.Graph(id='prediction-plot-podano')
+                ], style={'width': '45%', 'height': '35%'})
+            ])
+        ]
+    )
+
+
+def input_for_plots():
+    return html.Div(style={'display': 'inline-block'}, children=[
+        dcc.Dropdown(
+            id='specialities-dropdown-2',
+            options=[{'label': spec, 'value': spec} for spec in specialities['spec_full']],
+            value=specialities['spec_full'].iloc[91],
+            style={'width': '500px', 'margin': 'auto', 'border-radius': '5px'},
+            placeholder='Оберіть спеціальність'
+        ),
+        dcc.Input(
+            id='uni_code-2',
+            type='number',
+            value=28,
+            style={'width': '500px', 'margin': 'auto', 'border-radius': '5px'},
+            placeholder='Код ЗВО'
+        ),
+    ])
+
 
 app.layout = html.Div([
-    html.H1("Передбачення результатів вступної кампанії 2024", style={'text-align': 'center'}),
-
-    html.Div([
-        dcc.Dropdown(id='specialities-dropdown',
-                     options=[{'label': spec, 'value': spec} for spec in specialities['spec_full']],
-                     value=specialities['spec_full'].iloc[91],
-                     style={'width': '500px', 'margin': 'auto', 'background-color': 'rgba(46, 229, 157, 0.17)',
-                            'border-radius': '3px'},
-                     placeholder='Оберіть спеціальність'),
-    ], style={'text-align': 'center', 'margin-top': '20px', 'justify-content': 'center'}),
-
-    html.Div([
-        dcc.Input(id='podano_zayav', type='number', placeholder='Подано заяв', value=64),
-        dcc.Input(id='uni_code', type='number', placeholder='Код ЗВО', value=28),
-        dcc.Input(id='max_val', type='number', placeholder='Максимальний обсяг держзамовлення', value=43),
-        dcc.Input(id='super_val', type='number', placeholder='Суперобсяг', value=11328),
-        dcc.Input(id='kvota', type='number', placeholder='квота-1', value=0),
-    ], style={'text-align': 'center', 'margin': '10px'}),
-
-    html.Div([
-        html.Button('Спрогнозувати', id='predict-button', className='floating-button')],
-        style={'text-align': 'center', 'justify-content': 'center', 'margin-bottom': '10px'}),
-
-    html.Div(id='prediction-output-container',
-             style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center'},
-             children=[
-                 html.Div(id='prediction-output')]),
-
-    html.Table(id='plots-table', style={'display': 'none'}, children=[
-        html.Tr([
-            html.Td([
-                dcc.Graph(id='prediction-plot-total')
-            ], style={'width': '45%', 'height': '35%'}),
-            html.Td([
-                dcc.Graph(id='prediction-plot-podano')
-            ], style={'width': '45%', 'height': '35%'})
-        ])
-    ])
+    html.H1("Передбачення результатів вступної кампанії 2024", style={'text-align': 'center', 'margin': '20px'}),
+    html.Div("Введіть необхідні дані та натисніть кнопку нижче, щоб отримати прогноз результатів вступної кампанії:",
+             style={'text-align': 'center', 'margin': '10px'}),
+    input_fields_section(),
+    dropdown_section(),
+    button_section(),
+    prediction_output_section(),
+    input_for_plots(),
+    plots_table_section()
 ])
 
 
 @app.callback(
-    Output('plots-table', 'style'),
     Output('prediction-output', 'children'),
-    Output('prediction-plot-total', 'figure'),
-    Output('prediction-plot-podano', 'figure'),
     Input('predict-button', 'n_clicks'),
     State('specialities-dropdown', 'value'),
     State('uni_code', 'value'),
@@ -87,12 +164,11 @@ app.layout = html.Div([
     State('kvota', 'value'))
 def update_prediction(n_clicks, spec, uni_code, podano_zayav, max_val, super_val, kvota):
     if n_clicks is None:
-        return {'display': 'none'}, '', {}, {}
+        return ''
 
     spec_full = map_spec_full[map_spec_full['spec_full'] == spec].iloc[0, 1]
     uni_code = map_uni_code[map_uni_code['uni_code'] == uni_code].iloc[0, 1]
     authority = map_uni_to_authority[map_uni_to_authority['uni_code'] == uni_code].iloc[0, 1]
-    filtered_df = df[(df['spec_full'] == spec_full) & (df['uni_code'] == uni_code)]
 
     features = {
         'Подано заяв на бюджет': [int(podano_zayav)],
@@ -110,45 +186,39 @@ def update_prediction(n_clicks, spec, uni_code, podano_zayav, max_val, super_val
     res = loaded_model.predict(test_data)[0]
     prediction = str(int(res))
 
+    return prediction
+
+
+@app.callback(
+    Output('prediction-plot-total', 'figure'),
+    Output('prediction-plot-podano', 'figure'),
+    Input('specialities-dropdown-2', 'value'),
+    State('uni_code-2', 'value')
+)
+def generate_plots(speciality, uni_code):
+    filtered_df = df[(df['spec_full'] == speciality) & (df['uni_code'] == uni_code)]
+
     year_values = [year_to_smooth_mean[smooth_mean] for smooth_mean in filtered_df['Рік']]
-    year_values.append('2024')
 
     total_values = filtered_df['УСЬОГО'].tolist()
-    total_values.append(prediction)
-
     podano_values = filtered_df['Подано заяв на бюджет'].tolist()
-    podano_values.append(podano_zayav)
 
-    data = {
-        'Prediction': prediction,
-        'Рік': year_values,
-        'УСЬОГО': total_values,
-        'Подано заяв на бюджет': podano_values
-    }
+    plot_data_total = go.Scatter(x=year_values, y=total_values, mode='lines+markers', name='УСЬОГО')
 
-    plot_data_total = go.Scatter(x=data['Рік'], y=data['УСЬОГО'], mode='lines+markers', name='УСЬОГО')
-    last_data_point = (data['Рік'][-1], data['УСЬОГО'][-1])
-    annotation = go.layout.Annotation(
-        x=last_data_point[0],
-        y=int(last_data_point[1]) - 1,
-        text='Прогноз',
-        showarrow=False
-    )
     layout_total = go.Layout(title='УСЬОГО по рокам',
                              title_x=0.5,
                              xaxis=dict(title='Рік'),
-                             yaxis=dict(title='УСЬОГО'),
-                             annotations=[annotation])
+                             yaxis=dict(title='УСЬОГО'))
     fig_total = go.Figure(data=[plot_data_total], layout=layout_total)
 
-    plot_data_podano = go.Scatter(x=data['Рік'], y=data['Подано заяв на бюджет'],
+    plot_data_podano = go.Scatter(x=year_values, y=podano_values,
                                   mode='lines+markers', name='Подано заяв на бюджет')
     layout_podano = go.Layout(title='Подано заяв на бюджет по рокам', title_x=0.5, xaxis=dict(title='Рік'),
                               yaxis=dict(title='Подано заяв на бюджет'))
     fig_podano = go.Figure(data=[plot_data_podano], layout=layout_podano)
 
-    return {'display': 'inline-block'}, prediction, fig_total, fig_podano
+    return fig_total, fig_podano
 
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
